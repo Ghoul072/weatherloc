a4b958e3b590eabb989bf4aec794bdfd7cf1f514
Ghoul072 <rnaodm072@gmail.com>
2023-02-19 11:54:29 +0500

    update install_requires

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     long_description_content_type="text/markdown",
     long_description=long_description,
     packages=find_packages(),
-    install_requires=[],
+    install_requires=["requests"],
     keywords=['python', 'weather', 'geolocation', 'meteorology', 'api', 'weatherapi'],
     classifiers=[
         "Development Status :: 1 - Planning",
