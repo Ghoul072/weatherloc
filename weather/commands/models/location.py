41488769a956b0eeecf9d4799c4ff8f214d1e4fc
Ghoul072 <rnaodm072@gmail.com>
2023-02-20 08:25:49 +0500

    added variable documentation for location

--- weather/commands/models/location.py
+++ weather/commands/models/location.py
@@ -10,16 +10,20 @@ class Location:
         :param data: _description_
         :type data: dict
         """
-        self.name = data["name"]
-        self.region = data["region"]
-        self.lat = data["lat"]
-        self.latitude = self.lat
-        self.long = data["lon"]
-        self.longitude = self.long
+        self.name = data["name"]                                                #: Location name
+        self.region = data["region"]                                            #: Region or state of the location, if available
         try:
-            self.timezone_id = data["tz_id"]
-            self.localtime_epoch = data["localtime_epoch"]
-            self.localtime = dt.strptime(data["localtime"], "%Y-%m-%d %H:%M")
+            self.country = data["country"]                                      #: Location country
+        except KeyError:
+            self.country = self.name
+        self.lat = data["lat"]                                                  #: Latitude in decimal degree
+        self.latitude = self.lat                                                #: Alias for :var:`~weather.commands.models.location.Location.latitude`
+        self.long = data["lon"]                                                 #: Longitude in decimal degree
+        self.longitude = self.long                                              #: Alias for :var:`~weather.commands.models.location.Location.longitude`
+        try:
+            self.timezone_id = data["tz_id"]                                    #: Time zone name
+            self.localtime_epoch = data["localtime_epoch"]                      #: Local date and time in unix time
+            self.localtime = dt.strptime(data["localtime"], "%Y-%m-%d %H:%M")   #: Local date and time
         except KeyError:
             self.timezone_id = None
             self.localtime_epoch = None
