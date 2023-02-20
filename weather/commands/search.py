9dbd472f0ba84b3396db5b242e8f86ba8e5426e8
Ghoul072 <rnaodm072@gmail.com>
2023-02-20 10:13:25 +0500

    added search docs

--- weather/commands/search.py
+++ weather/commands/search.py
@@ -1,4 +1,12 @@
 from .models import Location
 
-def autocomplete(results: list) -> list:
+def autocomplete(results: list[dict]) -> list[Location]:
+    """
+    Parse search results
+
+    :param results: List of search results returned by the API
+    :type results: list[dict]
+    :return: list of :class:`~weather.commands.models.location.Location` objects
+    :rtype: list[Location]
+    """
     return [Location(result) for result in results]
