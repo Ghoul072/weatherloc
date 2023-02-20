3c0bc7738f7b4d7435f25e2b521cd2d822854863
Ghoul072 <rnaodm072@gmail.com>
2023-02-20 09:42:00 +0500

    added astro docs

--- weather/commands/models/astro.py
+++ weather/commands/models/astro.py
@@ -11,9 +11,9 @@ class Astro:
         :param data: Dict of data to be parsed
         :type data: dict
         """
-        self.sunrise = dt.strptime(data["sunrise"], "%I:%M %p").time()
-        self.sunset = dt.strptime(data["sunset"], "%I:%M %p").time()
-        self.moonrise = dt.strptime(data["moonrise"], "%I:%M %p").time()
-        self.moonset = dt.strptime(data["moonset"], "%I:%M %p").time()
-        self.moon_phase = data["moon_phase"]
-        self.moon_illumination = data["moon_illumination"]
+        self.sunrise = dt.strptime(data["sunrise"], "%I:%M %p").time()      #: Sunrise time as a datetime object
+        self.sunset = dt.strptime(data["sunset"], "%I:%M %p").time()        #: Sunset time as a datetime object
+        self.moonrise = dt.strptime(data["moonrise"], "%I:%M %p").time()    #: Moonrise time as a datetime object
+        self.moonset = dt.strptime(data["moonset"], "%I:%M %p").time()      #: Moonset time as a datetime object
+        self.moon_phase = data["moon_phase"]                                #: Name of moon phase
+        self.moon_illumination = data["moon_illumination"] / 100            #: Moon illumination as a decimal (0-1)
